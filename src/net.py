08db3695c1d29ffa3e37cd9040bf57a665db86a9
Liang Jiang <jal@mail.ustc.edu.cn>
2016-03-17 00:05:37 +0800

    add net.py to load net, usually for testing the correctness of prototxt

---
+++ src/net.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+caffe_root = '/usr/local/caffe/'
+import sys
+sys.path.insert(0, caffe_root + 'python')
+import caffe
+
+net = caffe.Net('models/train_val_fuse.prototxt', caffe.TRAIN)
+print 'shape of data:'
+for k, v in net.blobs.items():
+    print k, v.data.shape
+
+print 'shape of parmas:'
+for k, v in net.params.items():
+    print k, v[0].data.shape
