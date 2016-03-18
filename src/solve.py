b51cde90ef952f83f4f6f6d5906bd8f04940f2fd
Liang Jiang <jal@mail.ustc.edu.cn>
2016-03-18 21:14:57 +0800

    change loop size to display_size

--- src/solve.py
+++ src/solve.py
@@ -5,12 +5,14 @@ sys.path.insert(0, caffe_root + 'python')
 import caffe
 import numpy as np
 
-steps = 80000
+steps = 10000
 caffe.set_mode_gpu()
 caffe.set_device(0)
 train_loss = np.zeros(steps)
 solver = caffe.SGDSolver('/home/jiangliang/code/caffe_colorization/models/solver.prototxt')
-for i in range(steps):
-    solver.step(1) 
+display_size = 2
+for i in range(steps / display_size):
+    solver.step(display_size) 
     train_loss[i] = solver.net.blobs['loss'].data
 np.savetxt('/home/jiangliang/code/caffe_colorization/result/loss', train_loss)
+
