f1cc25347883a4dd4ce46467e7e98c66e7cb52b7
Liang Jiang <jal@mail.ustc.edu.cn>
2016-03-18 21:32:52 +0800

    fix solver.py does not block bug

--- src/run.py
+++ src/run.py
@@ -8,7 +8,7 @@ train_loss_string = '\"Train net output\"'
 val_loss_string = '\"Test net output\"'
 train_iter_str = "\"Iteration.*loss.*\""
 val_iter_str = "\"Iteration.*Testing.*\""
-child = subprocess.Popen('nohup ' + project_root + 'src/solve.py > ' + project_root + nohup_file + ' &', shell = True)
+child = subprocess.Popen(project_root + 'src/solve.py', shell = True)
 child.wait()
 
 def get_loss(loss_string, iter_string):
