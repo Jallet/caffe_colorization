c48bec09934f7e375d98dc7d6b615780f75410a4
Liang Jiang <jal@mail.ustc.edu.cn>
2016-03-31 14:16:58 +0800

    print out size of data, and number of parameters

--- src/net.py
+++ src/net.py
@@ -6,9 +6,21 @@ import caffe
 
 net = caffe.Net('models/train_val_fuse.prototxt', caffe.TRAIN)
 print 'shape of data:'
+total_size = 0
 for k, v in net.blobs.items():
-    print k, v.data.shape
+    size = 1
+    for i in range(len(v.data.shape)):
+        size = size * v.data.shape[i]
+    print("{}, {}, ({})".format(k, v.data.shape, size))
+    total_size = total_size + size
+print("total_size: {}".format(total_size))
 
-print 'shape of parmas:'
+print 'shape of params: '
+param_num = 0
 for k, v in net.params.items():
-    print k, v[0].data.shape
+    num = 1
+    for i in range(len(v[0].data.shape)):
+        num = num * v[0].data.shape[i]
+    print("{}, {}, ({})".format(k, v[0].data.shape, num))
+    param_num = param_num + num
+print "num of params: {}".format(param_num)
