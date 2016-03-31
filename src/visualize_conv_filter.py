02bf4f0312b739cfdd3b682cc31a43a86e7aa88c
Liang Jiang <jal@mail.ustc.edu.cn>
2016-03-31 14:33:52 +0800

    rename visualize_firt_conv_filter.py into visualize_conv_filter.py, modify the script to visualize the filters of a conv layer given the layer name

--- src/visualize_conv_filter.py
+++ src/visualize_conv_filter.py
@@ -11,27 +11,27 @@ def argparser():
     parser = argparse.ArgumentParser()
     parser.add_argument('network', help = 'Network to use')
     parser.add_argument('model', help = 'Model to use')
+    parser.add_argument('layer', help = 'which layer to visualize')
     return parser
 
 def vis_square(data):
-    data = data[0 : 61, :]
     data = (data - data.min()) / (data.max() - data.min())
     n = int(np.ceil(np.sqrt(data.shape[0])))
-    padding = ((0, n * n - data.shape[0]), (0, 0), (1, 1), (1, 1))
+    padding = ((0, 0), (0, 0), (1, 1), (1, 1))
     data = np.pad(data, padding, mode = 'constant', constant_values = 0)
-    data = data.squeeze()
+    #data = data.squeeze()
     total_array = np.array([])
-    for i in range(n):
+    for i in range(data.shape[0]):
         row_array = np.array([])
-        for j in range(n):
+        for j in range(data.shape[1]):
             if j == 0:
-                row_array = data[i * n + j, :, :]
+                row_array = data[i, j, :, :]
             else:
-                row_array = np.hstack((row_array, data[i * n + j, :, :]))
+                row_array = np.vstack((row_array, data[i, j, :, :]))
         if i == 0:
             total_array = row_array
         else:
-            total_array = np.vstack((total_array, row_array))
+            total_array = np.hstack((total_array, row_array))
     print("shape of total_array: {}".format(total_array.shape))
     #data = data.reshape((n, n) + data.shape[1:])
     #data = data.reshape((n * data.shape[1], n * data.shape[3]) + data.shape[4:])
@@ -40,11 +40,15 @@ def vis_square(data):
     plt.axis('off')
     plt.show()
 
-parser = argparser()
-args = parser.parse_args()
-network = args.network
-model = args.model
-net = caffe.Net(network, model, caffe.TEST)
-filters = net.params['conv1_1'][0].data
-vis_square(filters)
+def main():
+    parser = argparser()
+    args = parser.parse_args()
+    network = args.network
+    model = args.model
+    layer = args.layer
+    net = caffe.Net(network, model, caffe.TEST)
+    filters = net.params[layer][0].data
+    vis_square(filters)
 
+if __name__ == "__main__":
+    main()
